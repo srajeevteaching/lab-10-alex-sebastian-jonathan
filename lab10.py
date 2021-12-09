eaca40e0fecda9551b2f7250deaa5608acff0cc3
alexbean31 <atbean@loyola.edu>
2021-12-08 23:32:15 -0500

    included main

--- lab10.py
+++ lab10.py
@@ -20,13 +20,12 @@ def load_morse_dictionary(data_file):
     dictionary = {list_of_keys[i]: list_of_values[i] for i in range(len(list_of_keys))}
     return dictionary
 
-# def decode_file(dictionary, filename):
-#     listed_morse_code = []
-#     coded_file = open(filename)
-#     for line in coded_file:
-#         line_entry = line.split()
-#         listed_morse_code.extend(line_entry)
-
+def decode_file(dictionary, filename):
+    listed_morse_code = []
+    coded_file = open(filename)
+    for line in coded_file:
+        line_entry = line.split()
+        listed_morse_code.extend(line_entry)
 morse_dictionary = load_morse_dictionary("morsecode.txt")
 listed_morse_code = []
 listed_morse_decode = []
@@ -37,6 +36,7 @@ for line in coded_file:
 
 print(listed_morse_code)
 print(listed_morse_decode)
+
 
 def main():
     dictionary = load_morse_dictionary()
